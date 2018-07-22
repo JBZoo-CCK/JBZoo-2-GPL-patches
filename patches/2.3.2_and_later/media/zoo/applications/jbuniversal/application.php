e03a95810e41cbe6e58e14ea84eac69b817ed91f
SmetDenis <denis.smetannikov@item8.io>
2018-07-22 11:16:55 +0300

    Fix loader

--- patches/2.3.2_and_later/media/zoo/applications/jbuniversal/application.php
+++ patches/2.3.2_and_later/media/zoo/applications/jbuniversal/application.php
@@ -26,7 +26,7 @@ require_once __DIR__ . '/framework/jbzoo.php';
  */
 final class JBUniversalApplication extends Application
 {
-    const JBZOO_VERSION = '4.0.0';
+    const JBZOO_VERSION = '4.0.x';
 
     /**
      * @var bool
